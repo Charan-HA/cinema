f830dec3ab6bb7fac1283f65c7b4369c92f303b3
Charan-HA <charanha2001charu@gmail.com>
2024-07-29 19:42:14 +0530

    Complete Files

---
+++ src/pages/PageNotFound.js
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import PageNotFoundImage from "../assets/images/pagenotfound.png";
+import { Button } from "../components";
+import { useEffect } from "react";
+import { useTitle } from "../hooks/useTitle";
+
+export const PageNotFound = () => {
+
+  const pageTitle = useTitle(`Page not found`);
+
+  return (
+    <main>
+      <section className="flex flex-col justify-center px-2">
+        <div className="flex flex-col items-center my-4">
+          <p className="text-7xl text-gray-900 font-bold my-10 dark:text-white">404, Oops!..</p>
+          <div className="max-w-lg">
+            <img className="rounded" src={PageNotFoundImage} alt="page not found image" />
+          </div>
+        </div>
+        <div className="flex justify-center my-4">
+          <Link to="/">
+          <Button>Back To Cinemate</Button>
+          </Link>
+        </div>
+      </section>
+    </main>
+  )
+}
